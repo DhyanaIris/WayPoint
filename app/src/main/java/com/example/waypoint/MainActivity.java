205b9bfaa4777086d11fd6e8667cb91f15c5ef71
Mateus <mateus_castelanp@hotmail.com>
2024-05-10 11:03:59 -0300

    botao de cadastro agora leva pra pagina de cadastro

--- app/src/main/java/com/example/waypoint/MainActivity.java
+++ app/src/main/java/com/example/waypoint/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.waypoint;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView btnCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCadastro = findViewById(R.id.btnCadastro);
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
