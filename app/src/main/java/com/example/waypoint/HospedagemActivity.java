95c7864bc54cd6b256c8b18839c1db00864797e4
Mateus <mateus_castelanp@hotmail.com>
2024-05-18 15:04:59 -0300

    salvando hospadagem

--- app/src/main/java/com/example/waypoint/HospedagemActivity.java
+++ app/src/main/java/com/example/waypoint/HospedagemActivity.java
@@ -2,35 +2,60 @@ package com.example.waypoint;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.waypoint.database.dao.GasolinaDAO;
+import com.example.waypoint.database.dao.HospedagemDAO;
+import com.example.waypoint.database.model.GasolinaModel;
+import com.example.waypoint.database.model.HospedagemModel;
+
 public class HospedagemActivity extends AppCompatActivity {
 
+    private HospedagemDAO hospedagemDAO;
     private Button btnAddViagem, btnPularEtapa;
-    private EditText txtCustoNoite, txtTotalNoites, txtTotalQuartos, txtTotal;
+    private EditText txtCustoMedio, txtTotalNoites, txtTotalQuartos, txtTotal;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospedagem);
 
-        txtCustoNoite = findViewById(R.id.txtCustoNoite);
+        txtCustoMedio = findViewById(R.id.txtCustoMedio);
         txtTotalNoites = findViewById(R.id.txtTotalNoites);
         txtTotalQuartos = findViewById(R.id.txtTotalQuartos);
         txtTotal = findViewById(R.id.txtTotal);
+
+        TextWatcher watcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                calcularHospedagem();
+            }
+        };
+
+        txtCustoMedio.addTextChangedListener(watcher);
+        txtTotalNoites.addTextChangedListener(watcher);
+        txtTotalQuartos.addTextChangedListener(watcher);
 
 
         btnAddViagem = findViewById(R.id.btnAddViagem);
         btnAddViagem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it = new Intent(HospedagemActivity.this, DiversosActivity.class);
-                startActivity(it);
+                salvar();
             }
         });
 
@@ -42,5 +67,62 @@ public class HospedagemActivity extends AppCompatActivity {
                 startActivity(it);
             }
         });
+
+    }
+
+    private void salvar() {
+        hospedagemDAO = new HospedagemDAO(HospedagemActivity.this);
+        String custoMedio = String.valueOf(txtCustoMedio.getText()).trim();
+        String totalNoites = String.valueOf(txtTotalNoites.getText()).trim();
+        String totalQuartos = String.valueOf(txtTotalQuartos.getText()).trim();
+
+        if (custoMedio.isEmpty() || totalNoites.isEmpty() || totalQuartos.isEmpty()) {
+            Toast.makeText(HospedagemActivity.this, "Por favor, preencha todos os campos", Toast.LENGTH_SHORT).show();
+        }
+
+        float totalFloat = calcularHospedagem();
+
+        HospedagemModel hospedagemModel = new HospedagemModel();
+        hospedagemModel.setCustoMedio(Float.parseFloat(custoMedio));
+        hospedagemModel.setTotalNoites(Float.parseFloat(totalNoites));
+        hospedagemModel.setTotalQuartos(Float.parseFloat(totalQuartos));
+        hospedagemModel.setTotal(totalFloat);
+        hospedagemModel.setIdUsuario(MyApplication.getInstance().getIdUsuarioLogado());
+        hospedagemModel.setIdViagem(MyApplication.getInstance().getIdViagemAtual());
+
+        long rowId = hospedagemDAO.Insert(hospedagemModel);
+
+        if (rowId != -1) {
+            Intent it = new Intent(HospedagemActivity.this, TarifaAereaActivity.class);
+            startActivity(it);
+            finish();
+            Toast.makeText(HospedagemActivity.this, "Dados salvos com sucesso", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(HospedagemActivity.this, "Erro ao salvar os dados", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private float calcularHospedagem() {
+        try {
+            String custoNoite = String.valueOf(txtCustoMedio.getText()).trim();
+            String totalNoites = String.valueOf(txtTotalNoites.getText()).trim();
+            String totalQuartos = String.valueOf(txtTotalQuartos.getText()).trim();
+
+
+            if (custoNoite.isEmpty() || totalNoites.isEmpty() || totalQuartos.isEmpty()) {
+                return 0.0f;
+            }
+
+            float custoMedioFloat = Float.parseFloat(custoNoite);
+            float totalNoitesFloat = Float.parseFloat(totalNoites);
+            float totalQuartosFloat = Float.parseFloat(totalQuartos);
+
+            float total = (custoMedioFloat * totalNoitesFloat) * totalQuartosFloat;
+            txtTotal.setText(String.valueOf(total));
+            return total;
+        } catch (NumberFormatException e) {
+            txtTotal.setText("Valor Inválido");
+            return 0;
+        }
     }
 }
